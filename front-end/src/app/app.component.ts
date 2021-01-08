34638157aaf366124ec28182bbd54db1a85e07bb
Gabriel MG <gabmgdev@outlook.com>
2021-01-08 17:41:36 -0600

    Fin de Seccion 3

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,43 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  peliculasEnCines;
-  peliculasProximosEstrenos = [
-    {
-      titulo: 'Avengers: End Game',
-      fechaLanzamiento: new Date(),
-      precio: 1400.99,
-    },
-    {
-      titulo: 'Inception',
-      fechaLanzamiento: new Date('2016-11-14'),
-      precio: 300.99,
-    },
-    {
-      titulo: 'Rocky',
-      fechaLanzamiento: new Date('2016-11-14'),
-      precio: 300.99,
-    },
-  ];
+  constructor() {}
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculasEnCines = [
-        {
-          titulo: 'Spider-Man',
-          fechaLanzamiento: new Date(),
-          precio: 1400.99,
-        },
-        {
-          titulo: 'Moana',
-          fechaLanzamiento: new Date('2016-11-14'),
-          precio: 300.99,
-        },
-      ];
-    }, 500);
-  }
-
-  manejarRated(voto: number): void {
-    alert(voto);
-  }
+  ngOnInit(): void {}
 }
